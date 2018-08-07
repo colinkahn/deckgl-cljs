9e0b165e54b5ffb570f7641f25c5c516020fcd8a
Colin Kahn <info@colinkahn.com>
2018-08-06 22:33:49 -0700

    adding externs

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,3 +1,4 @@
 {:foreign-libs [{:file "deckgl/deckgl.min.js"
                  :provides ["com.uber.deckgl"]
-                 :global-exports '{com.uber.deckgl deck}}]}
+                 :global-exports '{com.uber.deckgl deck}}]
+ :externs ["deckgl/externs/deckgl.js"]}
